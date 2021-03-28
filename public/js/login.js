42bd2d1a94096c1acd755ae923a92358639708b2
tuktarov9898 <tuktarov2121@gmail.com>
2021-03-28 19:12:25 +0300

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -14,12 +14,19 @@ class LoginScreen extends Phaser.Scene{
             url: 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js',
             sceneKey: 'rexUI'
         });
+
+        this.load.plugin('rextexteditplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rextexteditplugin.min.js', true);
     }
 
     create() {
-        this.usernameText = this.rexUI.add.rexInputText(100, 200, 100, 50);
-        this.password = this.rexUI.add.rexInputText(100, 200, 100, 50);
+        this.userName = this.scene.add.text(100, 300, 'UserName', { fixedWidth: 150, fixedHeight: 36 });
+        // this.usernameText = this.rexUI.add.rexInputText(100, 200, 100, 50);
+        // this.password = this.rexUI.add.rexInputText(100, 200, 100, 50);
     
+        this.userName.setInteractive().on('pointerdown', () => {
+            this.rexUI.edit(text)
+        });
+
         this.helloButton = this.scene.add.text(100, 300, 'Hello Phaser!', this.onLogin, this, 2, 1, 0);
         helloButton.setInteractive();
     }
