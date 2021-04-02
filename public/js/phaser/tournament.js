2cda385a5b25eb5590ea6426113ce411e851dbd4
tuktarov9898 <tuktarov2121@gmail.com>
2021-04-02 13:41:52 +0300

    change

--- public/js/phaser/tournament.js
+++ public/js/phaser/tournament.js
@@ -6,8 +6,6 @@
 class TournamentScreen extends Phaser.Scene{
     constructor(){
         super({key: "TournamentScreen"});
-        this.userList = [];
-        this.pointList = [];
     }
 
     preload() {
@@ -19,72 +17,80 @@ class TournamentScreen extends Phaser.Scene{
     }
 
     create() {
-        this.userName = this.add.text(50, 100, userData.username, { fixedWidth: 100, fixedHeight: 36 });
-        
-        if(userData.username == tournamentData.userName)
-        {
-            this.startButton = this.add.text(150, 100, 'start');
-            this.startButton.setInteractive().on('pointerdown', () => {
-                Client.start();
-            });
-        }
-        else{
-            this.readyButton = this.add.text(150, 100, 'unReady');
-            this.readyButton.setInteractive().on('pointerdown', () => {
-                Client.ready();
-            });
-        }
+        this.tournament = this.add.text(150, 50, 'Tournament', { fixedHeight: 32 })
+        .setStyle({
+            fontSize: '32px',
+            fontFamily: 'Arial',
+            color: '#fdb63a',
+        }).setOrigin(0.5,0);
 
-        var plus=0;
-        if(userData.username != tournamentData.userName)
-        {
-            plus=1;
-            this.userList.push(this.add.text(50, 150, tournamentData.userName, { fixedWidth: 100, fixedHeight: 36 }));
-            this.stateList.push(this.add.text(150, 150, 'Creator', { fixedWidth: 100, fixedHeight: 36 }));
+        this.user_list = this.rexUI.add.scrollablePanel({
+            x: 150,
+            y: 400,
+            width: 200,
+            height: 150,
+
+            scrollMode: 0,
+
+            background: this.rexUI.add.roundRectangle(0, 0, 2, 2, 10, 0x4e342e),
+
+            panel: {
+                child: this.rexUI.add.fixWidthSizer({
+                    space: {
+                        left: 3,
+                        right: 3,
+                        top: 3,
+                        bottom: 3,
+                        item: 8,
+                        line: 8,
+                    }
+                }),
+
+                mask: {
+                    padding: 1
+                },
+            },
+
+            slider: {
+                track: this.rexUI.add.roundRectangle(0, 0, 20, 10, 10, 0x260e04),
+                thumb: this.rexUI.add.roundRectangle(0, 0, 0, 0, 13, 0x7b5e57),
+                input: 'drag',
+                position: 0,
+            },
+
+            scroller:{
+                threshold: 0,
+            },
+
+            clamplChildOY : true,
+
+            space: {
+                left: 10,
+                right: 10,
+                top: 10,
+                bottom: 10,
+
+                panel: 10,
+            }
+        });
+
+        var sizer = this.user_list.getElement('panel');
+        sizer.clear(true);
+
+        for(let i=0; i<tournamentData.length; i++){
+            sizer.add( this.add.text(0, 0, tournamentData[i].userName + ":" + tournamentData[i].point, { fontSize: 18 }));
         }
-        for(let i = 0; i<tournamentData.joinUsers.length; i++)
-        {
-            if(tournamentData.joinUsers[i].userName == userData.username)
-                continue;
-            this.userList.push(this.add.text(50, 100 + (i+1+plus)*50, tournamentData.joinUsers[i].userName, { fixedWidth: 100, fixedHeight: 36 }));
-            this.stateList.push(this.add.text(150, 100 + (i+1+plus)*50, tournamentData.joinUsers[i].isReady?'Ready' : 'unReady', { fixedWidth: 100, fixedHeight: 36 }));
-        }
+        this.user_list.layout();
+        this.user_list.setSliderEnable(true);
+        this.user_list.setScrollerEnable(true);
+        game.domContainer.style.display = 'none'
     }
     update(){
-        var plus=0;
-        if(userData.username != tournamentData.userName)
-        {
-            plus=1;
-        }
-        for(let i=0; i<tournamentData.joinUsers.length; i++)
-        {
-            if(tournamentData.joinUsers[i].userName == userData.username)
-            {
-                if(tournamentData.joinUsers[i].isReady)
-                {
-                    this.readyButton.setText('Ready');
-                    this.readyButton.setInteractive(false);
-                }
-                continue;
-            }
-            if(i>=this.userList.length-plus)
-            {
-                this.userList.push(this.add.text(50, 100 + (i+1+plus)*50, tournamentData.joinUsers[i].userName, { fixedWidth: 100, fixedHeight: 36 }));
-                this.stateList.push(this.add.text(150, 100 + (i+1+plus)*50, tournamentData.joinUsers[i].isReady?'Ready' : 'unReady', { fixedWidth: 100, fixedHeight: 36 }));
-                continue;
-            }
-            if(tournamentData.joinUsers[i].userName != this.userList[i+plus].text)
-            {
-                let deleted_item = this.userList.splice(i+plus);
-                deleted_item.destroy();
-                deleted_item = this.stateList.splice(i+plus);
-                deleted_item.destroy();
-                continue;
-            }
-            if(this.stateList[i+plus].text == 'unReady' && tournamentData.joinUsers[i].isReady)
-                this.stateList[i+plus].setText('Ready');
-            this.userList[i+plus].setPosition(50, 100 + (i+1+plus)*50);
-            this.stateList[i+plus].setPosition(150, 100 + (i+1+plus)*50);
-        }
+    }
+
+    add_user(added_user){
+        var sizer = this.user_list.getElement('panel');
+        sizer.add( this.add.text(0, 0, added_user.userName + ":" + added_user.point, { fontSize: 18 }));
+        this.user_list.layout();
     }
 }
