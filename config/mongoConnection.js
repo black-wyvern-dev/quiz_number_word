4befc914d38cd8e18c53584386b698192c89bce2
Alex1992818 <meanJustin@protonmail.com>
2021-04-06 19:44:20 +0900

    db

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -1,8 +1,10 @@
 const MongoClient = require('mongodb').MongoClient;
 const settings = {
     mongoConfig: {
-        serverUrl: 'mongodb://192.168.104.56:8002/',
-        database: 'Puzzle_number_word'
+        //serverUrl: 'mongodb://192.168.104.56:8002/',
+        //database: 'Puzzle_number_word'
+        serverUrl: 'mongodb+srv://admin:%21QAZxsw2@puzzle.am9gf.mongodb.net/test?authSource=admin&replicaSet=atlas-h19s4z-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+        database: 'Puzzle_word_number'
     }
 };
 const mongoConfig = settings.mongoConfig;
@@ -12,11 +14,16 @@ let _db = undefined;
 
 module.exports = async() => {
     if (!_connection) {
+	try {
         _connection = await MongoClient.connect(mongoConfig.serverUrl, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
         _db = await _connection.db(mongoConfig.database);
+	} catch (e) {
+	    console.log('Could not connect database');
+	    process.exit(0);
+	}
     }
 
     return _db;
