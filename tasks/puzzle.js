8f23ce51c95bfc257d7fc6c019a9306b30bf22b6
Vraj <tuktarov2121@gmail.com>
2021-05-01 04:31:36 +0900

    big number fix

--- tasks/puzzle.js
+++ tasks/puzzle.js
@@ -39,7 +39,7 @@ const generatedNumber = () => {
   let data = {array: []};
   for (let i = 0; i<5; i++)
     array.push(generateRandom(1, 9));
-  array.push(25*Math.ceil(generateRandom(26, 99)/25));
+  array.push(25*Math.ceil(generateRandom(26, 99)/25)-25);
   data.array = Array.from(array);
 
   let result = 0;
